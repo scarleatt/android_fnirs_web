130b176377cee9399f917ab5794dd0964ab6bd8b
resccu <fawnliu2333@gmail.com>
2018-03-12 18:13:29 +0800

    fix upload file

--- app/src/main/java/com/example/liufang/sqltest/MainActivity.java
+++ app/src/main/java/com/example/liufang/sqltest/MainActivity.java
@@ -1,19 +1,82 @@
 package com.example.liufang.sqltest;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
-public class MainActivity extends AppCompatActivity {
+import im.delight.android.webview.AdvancedWebView;
+
+
+public class MainActivity extends AppCompatActivity implements AdvancedWebView.Listener {
+    private AdvancedWebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WebView webview = (WebView) findViewById(R.id.web_view);
-        webview.getSettings().setJavaScriptEnabled(true);
-        webview.setWebViewClient(new WebViewClient());
-        webview.loadUrl("http://172.96.17.25/fnirs_web/");
+//        WebView webview = (WebView) findViewById(R.id.webview);
+//        webview.getSettings().setJavaScriptEnabled(true);
+//        webview.getSettings().setAllowFileAccess(true);
+//        webview.setWebViewClient(new WebViewClient());
+//        webview.loadUrl("http://172.96.17.25:3002/");
+
+        mWebView = (AdvancedWebView) findViewById(R.id.webview);
+        mWebView.setListener(this, this);
+        mWebView.loadUrl("http://172.96.17.25:3002/");
     }
+    @SuppressLint("NewApi")
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mWebView.onResume();
+        // ...
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    protected void onPause() {
+        mWebView.onPause();
+        // ...
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mWebView.onDestroy();
+        // ...
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        mWebView.onActivityResult(requestCode, resultCode, intent);
+        // ...
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!mWebView.onBackPressed()) { return; }
+        // ...
+        super.onBackPressed();
+    }
+
+    @Override
+    public void onPageStarted(String url, Bitmap favicon) { }
+
+    @Override
+    public void onPageFinished(String url) { }
+
+    @Override
+    public void onPageError(int errorCode, String description, String failingUrl) { }
+
+    @Override
+    public void onDownloadRequested(String url, String suggestedFilename, String mimeType, long contentLength, String contentDisposition, String userAgent) { }
+
+    @Override
+    public void onExternalPageRequest(String url) { }
+
+
 }
